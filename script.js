8891b0dec79a28752f2773587618ed4c88c65a0f
Pawel Paczkowski <paw.pac@yahoo.com>
2022-08-14 23:24:26 -0500

    Add event listeners to star-field

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const starField = document.getElementById('star-field');
+const elementForListener = document.querySelector('body');
 
-for (let i = 0; i < 10; i += 1) {
+for (let i = 0; i < 30; i += 1) {
   let starRow = document.createElement('div');
   // star.textContent = ".";
   starRow.classList.add('star-row');
@@ -10,11 +11,32 @@ for (let i = 0; i < 10; i += 1) {
 const starRows = document.querySelectorAll('.star-row');
 
 starRows.forEach((row) => {
-  for (let i = 0; i < 10; i += 1) {
+  for (let i = 0; i < 30; i += 1) {
     let star = document.createElement('div');
-    star.textContent = "*";
+    star.textContent = generateRandomNum();
     star.classList.add('star');
+    // star.style.padding = generateRandomNum();
     row.appendChild(star);
   }
 })
+
+elementForListener.addEventListener('mouseover', (e) => {
+ if (e.target.classList.contains('star')) {
+    e.target.classList.add('bright');
+ }
+})
+
+function generateRandomNum() {
+  let num =  Math.random();
+  if (num > .5) {
+    num = 1;
+  } else {
+    num = 0;
+  }
+  return num;
+}
+
+
+
+
 
